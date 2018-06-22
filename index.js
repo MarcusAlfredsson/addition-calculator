0859cb74d5d91da657470bf9424e74868a0ce5eb
Marcus Alfredsson <marcus@alfredo.se>
2018-06-22 18:42:37 +0200

    Added more styling and handle previous result

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
+var result = 0;
 function displayOutput(number) {
   let el = getOutputElement();
   el.value += number;
 }
 
 function calculate(stringValue) {
-  const stringValues = stringValue.split('').filter(a => (a !== '#' && a !== '*'));
+  const stringWithoutResult = stringValue.replace(`${result}`, '');
+  const stringValues = stringWithoutResult.split('').filter(a => (a !== '#' && a !== '*'));
   const values = stringValues.map(a => parseInt(a));
-  return values.reduce((a, b) => a+b);
+  let sumWithoutPreviousResult = 0;
+  if (values.length > 0) {
+    sumWithoutPreviousResult = values.reduce((a, b) => a+b);
+  }
+  result += sumWithoutPreviousResult
+  return result;
 }
 
 function calculateSum() {
@@ -23,4 +30,5 @@ function getOutputElement() {
 function resetOutput() {
   let el = getOutputElement();
   el.value = '';
+  result = 0;
 }
